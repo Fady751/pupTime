6c165fbbf4fe00d50bc07a0d102b95c3fd4fb507
Hany Maged <hany.maged9102@gmail.com>
2026-02-08 18:38:33 +0200

    Add custom user model and update settings for user app integration

--- Backend/user/models.py
+++ Backend/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    google_auth_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    gender = models.CharField(max_length=20, null=True, blank=True)
+    birth_day = models.DateField(null=True, blank=True)
+    streak_cnt = models.IntegerField(default=0)
+    joined_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
