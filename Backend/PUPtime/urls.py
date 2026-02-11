fbef9963a0dbfddec128bf7860798b9e7e76c7fd
Hany Maged <hany.maged9102@gmail.com>
2026-02-11 17:24:07 +0200

    Add a new endpoint for a simple "Hello, World!" response

--- Backend/PUPtime/urls.py
+++ Backend/PUPtime/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -20,4 +21,6 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('hello/', lambda request: HttpResponse("Hello, World!"))
+    ,
 ]
