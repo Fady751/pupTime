170f937399ed329c57b1c5c4c2c577ede1a9b9c8
Hany Maged <hany.maged9102@gmail.com>
2026-02-11 18:19:09 +0200

    Add static file handling to urlpatterns

--- Backend/PUPtime/urls.py
+++ Backend/PUPtime/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
@@ -21,6 +23,6 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('hello/', lambda request: HttpResponse("Hello, World :)!"))
-    ,
-]
+    path('hello/', lambda request: HttpResponse("Hello, World :)!")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
