413b8437164fe681ee82ba33d91a87590209b035
Hany Maged <hany.maged9102@gmail.com>
2026-02-25 18:02:19 +0200

    enable add id to post/ tasks

--- Backend/task/serializers.py
+++ Backend/task/serializers.py
@@ -11,7 +11,7 @@ class TaskOverrideSerializer(serializers.ModelSerializer):
             'id', 'instance_datetime', 'status',
             'new_datetime', 'created_at', 'updated_at',
         ]
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
 
 
 class InitialOverrideSerializer(serializers.Serializer):
