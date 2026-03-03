707947525a5f17a1772d04326fc6c0744f8cfdb9
Mechan <meral.mourad.24@gmail.com>
2026-03-03 03:31:16 +0200

    Tested frinedship's apis

--- Backend/friendship/views.py
+++ Backend/friendship/views.py
@@ -8,41 +8,56 @@ from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
+from .Backend import (
+    check_existing_friendship , 
+    get_user_by_id ,
+    get_friendship_by_id
+)
+
+from user.models import User
+from .models import Friendship
+
+from .models import Status
+
 from .serializers import (
-    FriendshipSerializer, FriendshipAcceptSerializer,
+    FriendshipRequestSerializer, FriendshipAcceptSerializer,
     FriendshipCancelRequestSerializer , BlockFriendshipSerializer 
     ,UnblockFriendshipSerializer
 )
 
 
-class FriendshipRequestView(generics.CreateAPIView):
-    serializer_class = FriendshipSerializer
-    permission_classes = [IsAuthenticated]  
+class FriendshipRequestView(APIView):
+    permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema(
-        request_body=FriendshipSerializer,
+        request_body=FriendshipRequestSerializer,
         responses={
-            201: openapi.Response('Friendship request created successfully', FriendshipSerializer),
+            201: openapi.Response('Friendship request created successfully', FriendshipRequestSerializer),
             400: openapi.Response('Bad request - validation errors or friendship already exists'),
         }
     )
-    def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
-    
-    def create(self, request, *args, **kwargs):
-        serializer_sender = self.get_serializer(data=request.data)
-        serializer_sender.is_valid(raise_exception=True)
-        user = serializer_sender.save()
-        token, _ = Token.objects.get_or_create(user=user)
-        headers = self.get_success_headers(serializer_sender.data)
-        response_data = serializer_sender.data
-        response_data['token'] = token.key
-        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
-    
-class FriendshipAcceptView(generics.UpdateAPIView):
-    serializer_class = FriendshipAcceptSerializer
-    permission_classes = [IsAuthenticated]
+    def post(self, request, user_id):
+        sender = request.user
+        
+        receiver = get_user_by_id(user_id)
+        
+        if re
 
+        existing_friendship = check_existing_friendship(sender.id, receiver.id) 
+    
+        if existing_friendship:
+            return Response({"error": "relation request already exists or requested." , "status": existing_friendship.status}, status=400)
+
+        serializer = FriendshipRequestSerializer(data={'sender': sender.id, 'receiver': receiver.id, 'status': Status.PENDING} , context={'request': request})
+        print(serializer.initial_data , "------------------------------------------------------")
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=201)
+    
+class FriendshipAcceptView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     @swagger_auto_schema(
         request_body=FriendshipAcceptSerializer,
         responses={
@@ -50,11 +65,12 @@ class FriendshipAcceptView(generics.UpdateAPIView):
             400: openapi.Response('Bad request - validation errors or not authorized to accept this request'),
         }
     )
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+    def post(self, request, friendship_id):
+        sender = request.user
+        friendship = get_object_or_404(Friendship, id=friendship_id)
+        serializer = FriendshipAcceptSerializer(friendship, data=request.data, partial=True, context={'request': request})
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        serializer.save()
         return Response(serializer.data)
 
 class FriendshipCancelRequestView(generics.DestroyAPIView):
@@ -75,39 +91,79 @@ class FriendshipCancelRequestView(generics.DestroyAPIView):
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)  
 
-class BlockFriendshipView(generics.UpdateAPIView):
-    serializer_class = BlockFriendshipSerializer
+class BlockFriendshipView(APIView):
     permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema(
-        request_body=BlockFriendshipSerializer,
+        request_body=None,
         responses={
             200: openapi.Response('User blocked successfully', BlockFriendshipSerializer),
-            400: openapi.Response('Bad request - validation errors or not authorized to block this user'),
+            400: openapi.Response('Bad request - validation errors or unauthorized action'),
         }
     )
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+    def post(self, request, user_id):
+        sender = request.user
+        
+        try:
+            receiver = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            existing_friendship = Friendship.objects.filter(
+                Q(sender=sender, receiver=receiver) | Q(sender=receiver, receiver=sender)).first()
+        except Friendship.DoesNotExist:
+            return Response({"error": "No existing relationship found"}, status=status.HTTP_404_NOT_FOUND)
+
+        if existing_friendship:
+            relationship = existing_friendship
+            serializer = BlockFriendshipSerializer(relationship, data={'status': Status.BLOCKED, 'blocked_by': sender.id}, partial=True , context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            data = serializer.data
+        else:
+            serializer = BlockFriendshipSerializer(data={'sender': sender.id, 'receiver': receiver.id, 'status': Status.BLOCKED, 'blocked_by': sender.id} , context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+           
+            data = serializer.data
+
+        return Response(
+            {
+                "message": "User blocked successfully",
+                "data": data
+            },
+            status=status.HTTP_200_OK
+        )
     
-class UnblockFriendshipView(generics.UpdateAPIView):
-    serializer_class = UnblockFriendshipSerializer
+class UnblockFriendshipView(APIView):
     permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema(
         request_body=UnblockFriendshipSerializer,
         responses={
             200: openapi.Response('User unblocked successfully', UnblockFriendshipSerializer),
-            400: openapi.Response('Bad request - validation errors or not authorized to unblock this user'),
+            400: openapi.Response('Bad request'),
         }
     )
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
-    
+    def delete(self, request, user_id):
+        sender = request.user
+
+        try:
+            receiver = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=404)
+
+        try:
+            friendship = Friendship.objects.get(sender=sender, receiver=receiver, status=Status.BLOCKED)
+        except Friendship.DoesNotExist:
+            return Response({"error": "can not unblock user you did not block him"}, status=404)
+
+        friendship.delete()
+
+        return Response(
+            {
+                "message": "User unblocked successfully"
+            },
+            status=status.HTTP_200_OK
+        )
