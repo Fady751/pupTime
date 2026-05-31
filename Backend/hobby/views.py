efd766aa8f81480e6f074705063570768ae016eb
Mechan <meral.mourad.24@gmail.com>
2026-05-31 22:32:50 +0300

    init

--- Backend/hobby/views.py
+++ Backend/hobby/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from grpc import Status
+from httpx import Response
+from pupTime.Backend import user
 from recommend.py import suggest_hobbies
-# Create your views here.
+from httpcore import request
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from django.db.models import Q
 
 
-suggest_hobbies(1 , 3)
+
+from friendship.models import Friendship
+from .serializers import HobbySerializer
+
+class FriendHobbyView (APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Response('hobbies returned successfully', HobbySerializer),
+            400: openapi.Response('Bad request - validation errors or unauthorized action'),
+        }
+    )
+
+
+    def get(self, request, start):
+       user = request.user
+       friends = Friendship.objects.filter(Q(sender=user) | Q(receiver=user), status=Status.ACCEPTED).order_by('-accepted_at')[start:start+3]
+       friends_ids = [f.receiver.id if f.sender == user else f.sender.id for f in friends]
+       user
+       
+
+
+
