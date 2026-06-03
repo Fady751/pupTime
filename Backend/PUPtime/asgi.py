40161533e9681077aa7ab1ab0cb7b391c462f6ee
Hany Maged <hany.maged9102@gmail.com>
2026-06-03 18:17:04 +0300

    Refactor ASGI application initialization and add librosa to requirements

--- Backend/PUPtime/asgi.py
+++ Backend/PUPtime/asgi.py
@@ -1,14 +1,15 @@
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PUPtime.settings')
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.middleware import TokenAuthMiddleware
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PUPtime.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
